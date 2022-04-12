3c8a14685f912bee99b81a2f2548b74df581e1a0
Aviran Benhamo <aviranbenhamo@gmail.com>
2022-04-12 10:33:34 +0200

    building models

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,7 +36,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+    # own
+    'application',
+    'bootcamp',
+    'bootcamp_location',
+    'bootcamp_student_relation',
+    'bootcamp_type',
+
+
+    #3rdparty
+    'model_utils',
+
+
+     ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
