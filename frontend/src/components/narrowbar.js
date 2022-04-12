7f473b116feef9aa4c651d017ed0ff640d5a4578
Michal Fugas <michal.fugas@hotmail.com>
2022-04-12 15:21:05 +0200

    bar components

--- frontend/src/components/narrowbar.js
+++ frontend/src/components/narrowbar.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import NarrowContainer from "../style/narrowbar.style"
 
 const NarrowBar =() =>{
-    
+    return(
+        <NarrowContainer>
+            <div>
+                <p>Max Mustermann</p>
+                <p>Full Stack</p>
+                <p>12.01.2022</p>
+                <p>Zurich</p>
+            </div>
+        </NarrowContainer>
+
+        
 
 
 
+
+    )
 }
+
+
+export default NarrowBar;
