c6d4ac986201aff0234264b07f191d4b85c68685
Aviran Benhamo <aviranbenhamo@gmail.com>
2022-04-13 16:17:04 +0200

    working on filtering

--- backend/bootcamps/serializers.py
+++ backend/bootcamps/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from applications.models import Application
 from bootcamps.models import Bootcamp
 
 
@@ -7,3 +9,19 @@ class BootcampSerializer(serializers.ModelSerializer):
         model = Bootcamp
         fields = '__all__'
         depth = 1
+
+
+class ApplicationUpComingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ('status',)
+
+
+class BootcampUpComingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bootcamp
+        fields = ('name', 'bootcamp_location', 'start_date', 'applications')
+    applications = ApplicationUpComingSerializer(read_only=True, many=True)
+
+
+
