7962afb8f1f22c02ad185b6303af1c34bd98a4b9
katya-rina <klolliger@gmail.com>
2022-04-28 15:35:18 +0200

    changed to review status

--- frontend/src/pages/applications.js
+++ frontend/src/pages/applications.js
@@ -145,7 +145,7 @@ const Applications = () => {
               <option value="enrolled">Enrolled</option>
               <option value="serious">Serious</option>
               <option value="accepted">Accepted</option>
-              <option value= "null" >To Review</option>
+              <option value= "to_review" >To Review</option>
               <option value="dropped_out">Dropped Out</option>
               <option value="not_serious">Not Serious</option>
             </select>
